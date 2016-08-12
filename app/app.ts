52b105d03758e4d1833803621931621ede4e4665
Maxim Kogan <maxim@elliptic.io>
2016-08-12 18:14:52 +0300

    Added Firebase package.

--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,8 @@ import { StatusBar } from 'ionic-native';
 import { Page1 } from './pages/page1/page1';
 import { Page2 } from './pages/page2/page2';
 
+import * as Firebase from 'firebase';
+
 @Component({
   templateUrl: 'build/app.html'
 })

742f2b66bbf30534517784d6332aa2d3cd5bddbc
Maxim Kogan <maxim@elliptic.io>
2016-08-13 14:52:56 +0300

    Visit website works.

--- app/pages/place-page/PlacePage.ts
+++ app/pages/place-page/PlacePage.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+declare var cordova;
+
 @Component({
   templateUrl: 'build/pages/place-page/place-page.html'
 })
@@ -9,5 +11,14 @@ export class PlacePage {
 
   constructor(public navCtrl: NavController, navParams: NavParams) {
     this.place = navParams.get('place');
+  }
+
+  visitWebsite () {
+    if (typeof cordova !== 'undefined') {
+      cordova.InAppBrowser.open(this.place.url, "_system", "location=true");
+    }
+    else {
+      window.location.href = this.place.url
+    }
   }
 }
